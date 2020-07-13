43c1fe6ca856301fdb8e44a37134e36981b1b67d
Shota Tamura <tamura@yuuniworks.com>
2020-07-13 17:31:43 +0900

    JWT

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,20 @@
-import { hash } from 'bcryptjs';
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import {
+  Arg,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from 'type-graphql';
 import { User } from './entity/User';
+
+@ObjectType()
+class LoginRespone {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -12,6 +26,29 @@ export class UserResolver {
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Mutation(() => LoginRespone) // 成功したかどうかを返す
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+  ): Promise<LoginRespone> {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw Error('could not find user');
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw Error('bad password');
+    }
+
+    return {
+      accessToken: sign({ userId: user.id }, 'asdfasdfasd', {
+        expiresIn: '15m',
+      }),
+    };
   }
 
   @Mutation(() => Boolean) // 成功したかどうかを返す
