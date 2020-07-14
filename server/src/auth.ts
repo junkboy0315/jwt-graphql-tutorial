d16c229470c120d77c9c3afe1429b7f39dd59685
Shota Tamura <tamura@yuuniworks.com>
2020-07-14 11:45:38 +0900

    dotenv

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -3,7 +3,7 @@ import { User } from './entity/User';
 
 export const createAccessToken = (user: User) => {
   return {
-    accessToken: sign({ userId: user.id }, 'asdfasdfasd', {
+    accessToken: sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
       expiresIn: '15m',
     }),
   };
@@ -11,7 +11,7 @@ export const createAccessToken = (user: User) => {
 
 export const createRefreshToken = (user: User) => {
   return {
-    accessToken: sign({ userId: user.id }, 'qwerqwer', {
+    accessToken: sign({ userId: user.id }, process.env.REFRESH_TOKEN_SECRET!, {
       expiresIn: '7d',
     }),
   };
